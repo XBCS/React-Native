02397bcef303d53fcfd6eb561ea909baa3a7ed0b
XBCS <xbcslzy@gmail.com>
2016-04-18 02:54:44 +0800

    完成初步布局

--- <4>MeiTuanHomePage/index.ios.js
+++ <4>MeiTuanHomePage/index.ios.js
@@ -14,13 +14,40 @@ import React, {
 class MeiTuanHomePage extends Component {
   render() {
     return (
-      <View>
+      <View style={[styles.height160, styles.row]}>
+          <View style={[styles.height160, styles.part_1_left]}>
+            <Text style={[styles.font30]}>1</Text>
+          </View>
+
+          <View style={[styles.height160, styles.part_1_right,]}>
+            <Text style={[styles.font30]}>2</Text>
+          </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+
+    row: {
+      flexDirection: 'row',
+    },
+    font30: {
+      fontSize: 90,
+    },
+    height160: {
+      height: 160
+    },
+    part_1_left: {
+      flex: 1,
+      borderWidth: 1,
+      borderColor: 'red'
+    },
+    part_1_right: {
+      flex: 2,
+      borderWidth: 1,
+      borderColor: 'red'
+    }
 
 });
 
