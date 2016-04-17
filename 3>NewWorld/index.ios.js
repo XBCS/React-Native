15e6b79476a8bee0f33cb8b3b1f7a710dbc079a2
XBCS <xbcslzy@gmail.com>
2016-04-18 00:17:28 +0800

    FlexBox布局之flex:当一个元素(组件)定义了flex属性,表示该元素(组件)是可伸缩的

--- 3>NewWorld/index.ios.js
+++ 3>NewWorld/index.ios.js
@@ -15,23 +15,23 @@ class NewWorld extends Component {
   render() {
     return (
       
-      <View>
-            <View style={styles.style_1}>
-
-            </View>
+      <View style={styles.style_0}>
+            <View style={styles.style_1}></View>
+            <View style={styles.style_1}></View>
+            <View style={{flex:10, borderWidth:1, borderColor: 'black'}}></View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  // style:{
-    // flex: 1,
-    // borderWidth: 1,  
-    // borderColor: 'red',
-  // },
+  style_0:{
+    flex: 1,
+    borderWidth: 1,  
+    borderColor: 'red',
+  },
   style_1:{
-    border: '1px solid red',
+    // border: '1px solid red',
     flex: 5,
     height: 40,
     borderWidth: 1,
